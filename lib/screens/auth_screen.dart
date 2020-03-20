123cb9a04d47af9adb5a0f0878d2bd4d3484125f
Dhruv Garg <garg.dh123@gmail.com>
2020-03-21 00:06:11 +0530

    first custom animation

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -94,7 +94,8 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   Map<String, String> _authData = {
@@ -103,6 +104,31 @@ class _AuthCardState extends State<AuthCard> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+  AnimationController _animationController;
+  Animation<Size> _heightAnimation;
+ 
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+
+    _heightAnimation = Tween<Size>(
+      begin: Size(double.infinity, 260),
+      end: Size(double.infinity, 320),
+    ).animate(CurvedAnimation(parent: _animationController, curve: Curves.linear));
+
+    _heightAnimation.addListener(() => setState(() {}));
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -111,7 +137,8 @@ class _AuthCardState extends State<AuthCard> {
               title: Text('Error'),
               content: Text(message),
               actions: <Widget>[
-                FlatButton(onPressed: () => Navigator.of(ctx).pop(), child: Text('Ok'))
+                FlatButton(
+                    onPressed: () => Navigator.of(ctx).pop(), child: Text('Ok'))
               ],
             ));
   }
@@ -160,7 +187,7 @@ class _AuthCardState extends State<AuthCard> {
       errorMessage = 'Could not Authenticate you. Please try again later.';
       _showErrorDialog(errorMessage);
     }
-    
+
     setState(() {
       _isLoading = false;
     });
@@ -171,10 +198,12 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _animationController.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _animationController.reverse();
     }
   }
 
@@ -187,7 +216,8 @@ class _AuthCardState extends State<AuthCard> {
       ),
       elevation: 8.0,
       child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // height: _authMode == AuthMode.Signup ? 320 : 260,
+        height: _heightAnimation.value.height,
         constraints:
             BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
         width: deviceSize.width * 0.75,
